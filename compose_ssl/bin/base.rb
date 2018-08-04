fe2f0fd7b30055e986768bb635eb34b427c03eea
andyl <andy@r210.com>
2018-08-04 15:46:20 -0700

    Update scripts

--- compose_ssl/bin/base.rb
+++ compose_ssl/bin/base.rb
@@ -12,9 +12,51 @@ class Hash
   end
 end
 
+class Domain
+  attr_reader :dom
+
+  def initialize(dom)
+    @dom = dom
+  end
+
+  def has_conf?
+    false
+  end
+
+  def has_cert?
+    false
+  end
+
+  def to_s
+    dom
+  end
+end
+
+class DomainPresenter
+  attr_reader :domain
+
+  def initialize(domain)
+    @domain = domain
+  end
+
+  def to_s
+    "#{conf.ljust(4)} #{cert.ljust(4)} #{domain}"
+  end
+
+  private
+
+  def conf
+    domain.has_conf? ? "X" : "-"
+  end
+
+  def cert
+    domain.has_cert? ? "X" : "-"
+  end
+end
+
 BASE = File.expand_path("../", __dir__)
 VOLS = BASE + "/volumes"
 DCOM = BASE + "/docker-compose.yml"
 YDAT = YAML::load_file(DCOM)
-DOMS = YDAT.deep_find("VIRTUAL_HOST")
+DOMS = YDAT.deep_find("VIRTUAL_HOST").map {|el| Domain.new(el)}
 puts DOMS
